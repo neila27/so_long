d8e57c4c67e43b150adac80c999c5d012587481e
Neila Muminovic <nmuminov@student.42lausanne.ch>
2023-05-25 16:36:36 +0200

    read_fd_map

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nmuminov <nmuminov@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 13:10:31 by nmuminov          #+#    #+#             */
-/*   Updated: 2023/05/24 18:10:14 by nmuminov         ###   ########.fr       */
+/*   Updated: 2023/05/25 16:35:42 by nmuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,29 @@ void	fail(char *str)
 	exit(1);
 }
 
+void	read_map(int *map)
+{
+	char	buff[255];
+	int 	fd;
+	char	*res;
+
+	res = NULL;
+	fd = open(*map, O_RDONLY);
+	int nbr = read(fd, buff, 255);
+	while (nbr)
+	{
+		res = ft_strjoin(res, buff);
+		nbr = read(fd, buff, 255);
+	}
+	close(fd);
+	return (res);
+}
 int	main(void)
 {
 	void	*mlx_wind;
 	void	*mlx;
-
 	
 	mlx = mlx_init();
-	mlx_wind = mlx_new_window(mlx, 640, 640, "hola");
+	mlx_wind = mlx_new_window(mlx, 640, 640, "game");
 	mlx_loop(mlx);
-
-
 }
-
